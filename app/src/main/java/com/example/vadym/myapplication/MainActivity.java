5cf5fa2f74b4047a61a60a9e13e1e2b47f8643ab
v.nevmerzhytskiy <v.nevmerzhytskiy@zinitsolutions.com>
2017-08-01 19:50:16 +0300

    review

--- app/src/main/java/com/example/vadym/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vadym/myapplication/MainActivity.java
@@ -4,26 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.Selection;
-import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
-import android.util.AttributeSet;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    private  EditText loginText;
+    private EditText loginText;
     private EditText passwordText;
     private CheckBox check;
     private Button buttonSignUp;
@@ -31,21 +23,28 @@ public class MainActivity extends AppCompatActivity  {
     private boolean mAutoSignIn;
     private SharedPreferences sharedPreferences;
 
+
+    // TODO: Розкинь по функціях свій функціонал, який я написав.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkinformation);
 
-        sharedPreferences = getSharedPreferences(Constants.AppPrefs, Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences(Constants.APP_PREFS, Context.MODE_PRIVATE);
 
         loginText = (EditText) findViewById(R.id.editLoginText);
         passwordText = (EditText) findViewById(R.id.editPasswordText);
         buttonSignUp = (Button) findViewById(R.id.button);
         check = (CheckBox) findViewById(R.id.checkBox);
 
-        mAutoSignIn = sharedPreferences.getBoolean(Constants.IS_Sign_In,false);
+        mAutoSignIn = sharedPreferences.getBoolean(Constants.IS_SIGN_IN, false);
 
         check.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // TODO: Я подумав, що сеттінг трансформМетода може зтирати позицію опточного курсора
+            // TODO: Тому, я її запам’ятовую тут...
+            int cursorPositionStart = passwordText.getSelectionStart();
+
             if (isChecked) {
                 passwordText.setTransformationMethod(null);
 
@@ -53,13 +52,15 @@ public class MainActivity extends AppCompatActivity  {
                 passwordText.setTransformationMethod(new PasswordTransformationMethod());
 
             }
-
+            // TODO: ...і ставлю назад тут. Вуаля.
             //Устанавливает курсор в конце слова
-            //passwordText.setSelection(passwordText.getText().length());
+            passwordText.setSelection(cursorPositionStart);
         });
 
         buttonSignUp.setOnClickListener(v -> submitInformation());
 
+
+        // TODO: Це можна зтерти, воно не потрібне.
         passwordText.setOnTouchListener((v, event) -> {
 
             // TODO: не працює запам’ятовування позиції курсора, коли знімаємо/ставимо галочку, весь час курсор падає на початок слова.
@@ -73,35 +74,67 @@ public class MainActivity extends AppCompatActivity  {
         });
 
 
-
         passwordText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction()==KeyEvent.ACTION_DOWN){
-                    switch (keyCode){
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
                         case KeyEvent.KEYCODE_DPAD_CENTER:
                         case KeyEvent.KEYCODE_ENTER:
                             submitInformation();
-                            return  true;
+                            return true;
                         default:
                             return false;
                     }
                 }
-                return  false;
+                return false;
             }
         });
 
-
-        if(mAutoSignIn){
-            Intent intent = new Intent(this,OpenNewWindow.class);
+        // TODO: Тут гарно зробив, але в нас проблема є в тому, що є функціонал на цьому екрані, який є ідентичним.
+        // TODO: Функція #goToNextActivity() має той самий код як знизу, тільки там ще викликається saveLoginAndPassword.
+        // TODO: Спробуй трохи інакше написати цей код.
+        if (mAutoSignIn) {
+            Intent intent = new Intent(this, OpenNewWindow.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             finish();
         }
 
+
+
+
+
+
+
+
+
+
+
+
+        init();
+        initOnClickListeners();
+        initViewsListeners();
+    }
+
+
+
+    private void init(){
+        // TODO: Сиди напиши findViewById всі і перевірку на прапор IS_AUTO_SING_IN.
+        // TODO: Дивись, ми цим вбиваємо двох зайців(щодо другого).
+        // TODO: Якщо цей прапор в тру, то нам треба кинути на другу актівіті, інші дії з елементами ж не тре робити.
+        // TODO: Якщо ні, то в ініт інших елементів воно саме зайде далі, оскільки викликаються всі функції.
+    }
+
+    private void initOnClickListeners(){
+        // TODO: Тут всі setOnClickListener`и напиши.
+    }
+    private void initViewsListeners(){
+        // TODO: А тут всі інші listeners.
     }
 
     public void submitInformation() {
+        // TODO: Поки це не чіпай, але хай буде.
         // TODO: Коли стою на логіні, є введений якийсь текст і пустий пароль - то повідомлення помилки не показується.
         // TODO: Треба ховати помилки, якщо продовжуємо писати текст, тому що овни не зникаєть, поки не натиснеш ентер.
         if (loginText.getText().length() == 0 && passwordText.getText().length() == 0) {
@@ -118,7 +151,7 @@ public class MainActivity extends AppCompatActivity  {
             passwordText.setError(getString(R.string.password_error));
         } else {
 
-            goNextActivity();
+            goToNextActivity();
 
             /*new AlertDialog.Builder(this)
                     .setTitle(R.string.final_title)
@@ -127,28 +160,27 @@ public class MainActivity extends AppCompatActivity  {
         }
     }
 
-    private void setSettingsToSharedPreferencesEditor(){
-
+    // TODO: Краще назвати функцію ясніше, типу так ->
+//    private void setSettingsToSharedPreferencesEditor(){
+    private void saveLoginAndPassword() {
         String log = loginText.getText().toString();
         String pas = passwordText.getText().toString();
 
         SharedPreferences.Editor edit = sharedPreferences.edit();
 
-        edit.putString(Constants.Login, log);
-        edit.putString(Constants.Password,pas);
-        edit.putBoolean(Constants.IS_Sign_In,true);
+        edit.putString(Constants.LOGIN, log);
+        edit.putString(Constants.PASSWORD, pas);
+        edit.putBoolean(Constants.IS_SIGN_IN, true);
         edit.apply();
-
     }
 
-    private void goNextActivity(){
-        setSettingsToSharedPreferencesEditor();
+    private void goToNextActivity() {
+        saveLoginAndPassword();
 
-        Intent intent = new Intent(this,OpenNewWindow.class);
+        Intent intent = new Intent(this, OpenNewWindow.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
-
     }
 
 }
--- app/src/main/java/com/example/vadym/myapplication/OpenNewWindow.java
+++ app/src/main/java/com/example/vadym/myapplication/OpenNewWindow.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,8 +24,8 @@ public class OpenNewWindow extends AppCompatActivity {
         welcomeText = (TextView) findViewById(R.id.welcomeText);
         btnExit = (Button) findViewById(R.id.btnExit);
 
-        sharedPreferences = getSharedPreferences(Constants.AppPrefs, Context.MODE_PRIVATE);
-        String user = sharedPreferences.getString(Constants.Login,"");
+        sharedPreferences = getSharedPreferences(Constants.APP_PREFS, Context.MODE_PRIVATE);
+        String user = sharedPreferences.getString(Constants.LOGIN,"");
         welcomeText.setText("Hello " + user);
 
         btnExit.setOnClickListener(v -> logout());
